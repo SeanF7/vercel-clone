c3fb130aa452e4a37ca972b8fd4fb550aa6268d4
Sean Firsching <seanfirsching@gmail.com>
2023-11-08 17:43:19 -0500

    Add menu popup event listeners and fix menu display bug

--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -1,11 +1,12 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { usePathname } from "next/navigation";
 import { DesktopNavPopup } from "./DesktopNavPopup";
 import { MobileNavPopup } from "./MobileNavPopup";
 
 export const NavButton = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const menuPopup = useRef<HTMLDivElement>(null);
   const pathname = usePathname();
 
   const handleButtonClick = () => {
@@ -16,7 +17,6 @@ export const NavButton = () => {
   };
   const [width, setWidth] = useState(0);
 
-  // Makes sure to drop the menu when the switching between mobile and desktop views.
   useEffect(() => {
     const handleResize = () => {
       if (
@@ -35,9 +35,34 @@ export const NavButton = () => {
     };
   }, [width]);
 
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setShowMenu(false);
+      }
+    };
+
+    const handleClick = (event: MouseEvent) => {
+      if (
+        menuPopup.current &&
+        !menuPopup.current.contains(event.target as Node)
+      ) {
+        setShowMenu(false);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("mousedown", handleClick);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, []);
+
   return (
     <div className="relative">
-      <button className="flex h-8 w-8 " onClick={handleButtonClick}>
+      <button className="flex h-8 w-8" onClick={handleButtonClick}>
         <div className="hidden h-full w-full rounded-full bg-gradient-to-r from-green-400 to-blue-500 [@media(min-width:700px)]:flex" />
         {showMenu ? (
           <div className="hidden h-full w-full items-center justify-center rounded-full border border-gray-800 [@media(max-width:699px)]:flex">
@@ -51,9 +76,10 @@ export const NavButton = () => {
           </div>
         )}
       </button>
-      {/** This menu should be full screen when width is <= 700 */}
-      {showMenu && width > 700 && <DesktopNavPopup pathname={pathname} />}
-      {showMenu && width <= 700 && <MobileNavPopup />}
+      <div ref={menuPopup}>
+        {showMenu && width > 700 && <DesktopNavPopup pathname={pathname} />}
+        {showMenu && width <= 700 && <MobileNavPopup />}
+      </div>
     </div>
   );
 };
