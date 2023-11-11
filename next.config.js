1e9c3939a4394ce04b4a89e37d857f1554451ba7
Sean Firsching <seanfirsching@gmail.com>
2023-11-10 23:48:51 -0500

    Setup redirect.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["localhost", "avatar.vercel.sh", "assets.vercel.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
