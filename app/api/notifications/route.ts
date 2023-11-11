6df363b3dc3647d5780d048a91c52ac14ea775c7
Sean Firsching <seanfirsching@gmail.com>
2023-11-10 21:03:34 -0500

    Reset Notifications every 5 minutes.

--- app/api/notifications/route.ts
+++ app/api/notifications/route.ts
@@ -6,45 +6,59 @@ type Notification = {
   description: string;
   time: string;
 };
-const notifications: Notification[] = [
-  {
-    id: 3,
-    image: "https://avatar.vercel.sh/seanfirsching",
-    description: "Sean Firsching followed you",
-    time: "2h ago",
-  },
-  {
-    id: 4,
-    image: "https://avatar.vercel.sh/seanfirsching",
-    description: "Sean Firsching followed you",
-    time: "2h ago",
-  },
-];
-const archivedNotifications: Notification[] = [
-  {
-    id: 1,
-    image: "https://avatar.vercel.sh/seanfirsching",
-    description: "Sean Firsching followed you",
-    time: "1h ago",
-  },
-  {
-    id: 2,
-    image: "https://avatar.vercel.sh/seanfirsching",
-    description: "Sean Firsching followed you",
-    time: "1h ago",
-  },
-];
+
+let inbox: Notification[] = generateInbox();
+let archive: Notification[] = generateArchive();
+
 export async function GET(request: NextRequest) {
   const isArchived = Boolean(request.nextUrl.searchParams.get("archived"));
-  console.log(isArchived, request.nextUrl.searchParams.get("archived"));
-  if (isArchived) return Response.json(archivedNotifications);
-  return Response.json(notifications);
+  if (isArchived) return Response.json(archive);
+  return Response.json(inbox);
 }
 
 export async function DELETE(request: NextRequest) {
   const id = Number(request.nextUrl.searchParams.get("id"));
-  const index = notifications.findIndex((project) => project.id === id);
-  archivedNotifications.push(notifications[index]);
-  notifications.splice(index, 1);
-  return Response.json(notifications);
+  const index = inbox.findIndex((project) => project.id === id);
+  archive.push(inbox[index]);
+  inbox.splice(index, 1);
+  return Response.json(inbox);
+}
+
+setInterval(() => {
+  inbox = generateInbox();
+  archive = generateArchive();
+}, 300000);
+
+function generateInbox() {
+  return [
+    {
+      id: 3,
+      image: "https://avatar.vercel.sh/seanfirsching",
+      description: "Sean Firsching followed you",
+      time: "2h ago",
+    },
+    {
+      id: 4,
+      image: "https://avatar.vercel.sh/seanfirsching",
+      description: "Sean Firsching followed you",
+      time: "2h ago",
+    },
+  ];
+}
+
+function generateArchive() {
+  return [
+    {
+      id: 1,
+      image: "https://avatar.vercel.sh/seanfirsching",
+      description: "Sean Firsching followed you",
+      time: "1h ago",
+    },
+    {
+      id: 2,
+      image: "https://avatar.vercel.sh/seanfirsching",
+      description: "Sean Firsching followed you",
+      time: "1h ago",
+    },
+  ];
 }
