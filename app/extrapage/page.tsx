b1dfb674657b5622fd98535d92c43610603de345
Sean Firsching <seanfirsching@gmail.com>
2023-11-19 22:47:21 -0500

    Oops page needed stuff,

---
+++ app/extrapage/page.tsx
@@ -0,0 +1,11 @@
+import { NavBar } from "@/components/NavBar/NavBar";
+import { ProjectsContent } from "@/components/Projects/ProjectsContent";
+import { ProjectWrapper } from "@/components/Projects/ProjectWrapper";
+
+export default function Home() {
+  return (
+    <main>
+      <NavBar />
+    </main>
+  );
+}
