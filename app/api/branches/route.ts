824bb8ebdcfb99d1fd541900604a2725da1b4cee
Sean Firsching <seanfirsching@gmail.com>
2023-11-20 22:06:16 -0500

    Fixed branch and page search.

--- app/api/branches/route.ts
+++ app/api/branches/route.ts
@@ -4,6 +4,9 @@ import { branches } from "@/lib/utils/fakeDatabase";
 export async function GET(request: NextRequest) {
   let search = request.nextUrl.searchParams.get("q");
   if (!search) return Response.json(branches);
-  branches.filter((branch) => branch.branchName.includes(search!));
-  return Response.json(branches);
+  return Response.json(
+    branches.filter((branch) =>
+      branch.branchName.toLowerCase().includes(search!.toLowerCase())
+    )
+  );
 }
--- app/api/pages/route.ts
+++ app/api/pages/route.ts
@@ -4,6 +4,9 @@ import { pages } from "@/lib/utils/fakeDatabase";
 export async function GET(request: NextRequest) {
   let search = request.nextUrl.searchParams.get("q");
   if (!search) return Response.json(pages);
-
-  return Response.json(pages.filter((page) => page.pageName.includes(search!)));
+  return Response.json(
+    pages.filter((page) =>
+      page.pageName.toLowerCase().includes(search!.toLowerCase())
+    )
+  );
 }
